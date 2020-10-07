e65f8c2cb139db1bcec65b9119c4bb506563d6f5
Ajeya <01fe19bcs174@kletech.ac.in>
2020-10-07 22:41:33 +0530

    added tooltip and naive search language function

--- src/components/repoCard.js
+++ src/components/repoCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Card, Grid, Text, Box } from 'theme-ui'
-import CodeIcon from "../images/icons/code.inline.svg"
 import GithubIcon from "../images/icons/github.inline.svg"
+import CodeIcon from "../images/icons/code.inline.svg"
 import IssueIcon from "../images/icons/issue.inline.svg"
 import StarIcon from "../images/icons/star.inline.svg"
-
+import './layout.css'
 const openGithubPage = (githubLink) => {
   window.open(githubLink, '_blank');
 };
@@ -51,20 +51,224 @@ const RepoCard = ({ repo }) => {
         <Text><span role="img" aria-label="star" style={{ verticalAlign: 'middle' }}>
           <StarIcon />
         </span> {repo.stargazers_count}</Text>
-        <Text><span role="img" aria-label="issues" style={{ verticalAlign: 'middle' }}>
+        <Text><span class="tooltip" role="img" aria-label="issues" style={{ verticalAlign: 'middle' }}>
           <IssueIcon />
+          <span class="tooltiptext">Open Issues</span>
         </span> {repo.open_issues_count}</Text>
         <a href={repo.html_url} style={{ color: 'currentColor' }}>
           <Text><span role="img" aria-label="github" style={{ verticalAlign: 'middle' }}>
             <GithubIcon />
           </span> Github</Text>
         </a>
-        <Text><span role="img" aria-label="code" style={{ verticalAlign: 'middle' }}>
-          <CodeIcon />
-        </span> {repo.language}</Text>
+        <Text>
+          <span class="tooltip" role="img" aria-label="language" style={{ verticalAlign: 'middle' }}>
+          {naiveCheckLang()}{checkNullLang()}            
+          </span>
+        </Text>
       </Grid>
     </Card>
   )
+  function checkNullLang()
+  {
+    if(repo.language!=null)
+      return(<span class="tooltiptext">{repo.language}</span>)
+  }
+  /*Naive Check Language function to siplay icons of programming languages*/
+  //ineffeciant and some possible changes needed
+  // switch implementation?
+  // b/w logos for better ui?
+  function naiveCheckLang()
+  {
+    if(repo.language==='Abap')
+    {
+    return(
+      <img width="16px" height="16px" src={require("../images/icons/languages/Abap.png")} alt="Abap"/>
+        )
+    } 
+    else if(repo.language==='Ada')
+    {
+    return(
+      <img width="16px" height="16px" src={require("../images/icons/languages/Ada.png")} alt="Ada"/>
+           )
+    }
+    else if(repo.language==='C'||repo.language==='C++')
+    {
+    return(
+       <img width="16px" height="16px" src={require("../images/icons/languages/Cpp.svg")} alt="C/C++"/>
+         )
+    }
+    else if(repo.language==='Cobol')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Cobol.png")} alt="Cobol"/>
+       )
+    }
+    else if(repo.language==='C#')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Csharp.svg")} alt="C#"/>
+     )
+    }
+    else if(repo.language==='Dart')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Dart.svg")} alt="Dart"/>
+    )
+    }
+    else if(repo.language==='Delphi')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Delphi.png")} alt="Delphi"/>
+    )
+    }    
+    else if(repo.language==='Go')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Go.png")} alt="Go"/>
+    )
+    }
+    else if(repo.language==='Groovy')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Groovy.png")} alt="Groovy"/>
+    )
+    }
+    else if(repo.language==='Haskell')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Haskell.svg")} alt="Haskell"/>
+    )
+    }
+    else if(repo.language==='Julia')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Julia.png")} alt="Julia"/>
+    )
+    }
+    else if(repo.language==='Java')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Java.svg")} alt="Java"/>
+    )
+    }
+    else if(repo.language==='Kotlin')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Kotlin.svg")} alt="Kotlin"/>
+    )
+    }
+    else if(repo.language==='Lua')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Lua.svg")} alt="Lua"/>
+    )
+    }
+    else if(repo.language==='Javascript')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Javascript.svg")} alt="Javascript"/>
+    )
+    }
+    else if(repo.language==='Rust')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Rust.png")} alt="Rust"/>
+    )
+    }
+    else if(repo.language==='Python')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Python.svg")} alt="Python"/>
+    )
+    }
+    else if(repo.language==='PHP')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/PHP.svg")} alt="PHP"/>
+    )
+    }
+    else if(repo.language==='Matlab')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Matlab.png")} alt="Matlab"/>
+    )
+    }
+    else if(repo.language==='R')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/R.svg")} alt="R"/>
+    )
+    }
+    else if(repo.language==='Ruby')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Ruby.svg")} alt="Ruby"/>
+    )
+    }
+    else if(repo.language==='Vue')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Vue.png")} alt="Vue"/>
+    )
+    }
+    else if(repo.language==='VBA')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/VBA.png")} alt="VBA"/>
+    )
+    } 
+    else if(repo.language==='Visual Basic')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Visual_Basic.jpg")} alt="Visual Basic"/>
+    )
+    }
+    else if(repo.language==='Typescript')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Typescript.svg")} alt="Typescript"/>
+    )
+    }
+    else if(repo.language==='Swift')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Swift.svg")} alt="Swift"/>
+    )
+    }
+    else if(repo.language==='Perl')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Perl.png")} alt="Perl"/>
+    )
+    }
+    else if(repo.language==='Scala')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Scala.png")} alt="Scala"/>
+    )
+    }
+    else if(repo.language==='Objective-C')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Objective_C.png")} alt="Objectve-C" />
+    )
+    }
+    else if(repo.language==='TeX')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/TeX.png")} alt="TeX" />
+    )
+    }
+    else if(repo.language==='Jupyter Notebook')
+    {
+    return(
+        <img width="16px" height="16px" src={require("../images/icons/languages/Jupyter_Notebook.png")} alt="Jupyter Notebook" />
+    )
+    }
+    else
+    return(
+      <CodeIcon />
+    )
+  }
 }
-
 export default RepoCard
