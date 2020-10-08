af9e7e2766f52f987d9485932889d1445709b960
ShadowLegend <lyheng0@gmail.com>
2020-10-09 00:26:13 +0700

    comment cy.waitForReact and update some tests

--- cypress/e2e/index.test.js
+++ cypress/e2e/index.test.js
@@ -12,19 +12,23 @@ describe('home page', () => {
 describe('home page search bar', () => {
   beforeEach(() => {
     cy.visit('/')
-    cy.waitForReact()
+    // cy.waitForReact()
   })
 
   it('should update corresponding search state', () => {
     cy.get('input[placeholder="Search Covid-19 related repos"]:first').type('covid19')
     cy.get('select:has(option:contains("Sort by Stars")):first').select('Sort by Best Match')
     cy.get('select:has(option:contains("All Languages")):first').select('Dart')
+
+    cy.get('input[placeholder="Search Covid-19 related repos"]:first').should('have.value', 'covid19')
+    cy.get('select:has(option:contains("Sort by Stars")):first').should('have.value', '')
+    cy.get('select:has(option:contains("All Languages")):first').should('have.value', 'Dart')
   })
 
   it('should only list repo that filtered with certain language', () => {
     cy.get('select:has(option:contains("All Languages")):first').select('Dart')
     cy.get('input[placeholder="Search Covid-19 related repos"]:first').parent('div').get('svg:eq(0)').click()
-    cy.get('div:contains("Dart")').should('have.length.at.least', 16)
+    cy.get('main div a').parent('div').children('div:contains("Dart")').should('have.length', 30)
   })
 
   it('should not show on navbar in smaller screen size and instead show button to toggle modal with search', () => {
@@ -47,5 +51,6 @@ describe('home page search bar', () => {
     cy.get('button[name="toggle-modal-with-search"]').click()
     cy.get('input[placeholder="Search Covid-19 related repos"]:eq(1)').type('flutter')
     cy.get('input[placeholder="Search Covid-19 related repos"]:eq(1)').parent('div').children('svg:eq(0)').click()
+    cy.get('main div a').parent('div').children('div:contains("Dart")').should('have.length', 30)
   })
 })
