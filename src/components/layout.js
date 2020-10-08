e4e285dadbe3b0b7cf352ea148eda0817e9acb41
ShadowLegend <lyheng0@gmail.com>
2020-10-08 17:12:44 +0700

    pass isShowModal prop to layout to have blur effect when modal is on

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@ import Footer from './footer'
 
 import './layout.css'
 
-const PageLayout = ({ data, children, isShowSearch, searchCompProps }) => {
+const PageLayout = ({ data, children, isShowModal, isShowSearch, toggleModal, searchCompProps }) => {
   const siteData = data.siteData
 
   return (
@@ -15,12 +15,14 @@ const PageLayout = ({ data, children, isShowSearch, searchCompProps }) => {
         flexDirection: 'column',
         height: '100vh',
         overflow: 'hidden',
-        backgroundColor: `#303030`
+        backgroundColor: `#303030`,
+        filter: `blur(${isShowModal ? 3 : 0}px)`,
       }}
     >
       <Header
         sx={{
         }}
+        toggleModal={toggleModal}
         isShowSearch={isShowSearch}
         searchCompProps={searchCompProps}
         title={siteData.siteMetadata.description} />
