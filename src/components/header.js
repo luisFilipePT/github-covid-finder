53ee3fe02cc0cec22ddae211d61b2ef1c1d5e181
ShadowLegend <lyheng0@gmail.com>
2020-10-08 14:56:45 +0700

    pass search comp prop to search component and conditionally check if should show search

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
+<<<<<<< HEAD
 import { Box, Flex, Text, Image, useColorMode, Button } from 'theme-ui'
 import logoWhite from '../../src/images/logo-white.svg'
 import logoBlack from '../../src/images/logo.svg'
 import GithubIcon from "../images/icons/github.inline.svg"
 const Header = () => {
   const [colorMode, setColorMode] = useColorMode()
+=======
+import { Box, Flex, Text, Image } from 'theme-ui'
+
+import Search from './search'
+import mainLogo from '../../src/images/logo-white.svg'
+import GithubIcon from "../images/icons/github.inline.svg"
+
+const Header = ({ isShowSearch, searchCompProps }) => {
+>>>>>>> pass search comp prop to search component and conditionally check if should show search
   return (
     <Box
       sx={{
@@ -17,7 +27,7 @@ const Header = () => {
       <Flex
         as="header"
         sx={{
-          height: '80px',
+          height: '120px',
           alignItems: 'center',
           justifyContent: 'space-between',
           margin: '0 auto',
@@ -25,19 +35,54 @@ const Header = () => {
           px: '15px',
         }}
       >
-        <Text
+        <Flex
           sx={{
-            fontSize: '24px',
-            color: 'text',
-            fontFamily: 'inter',
-            textAlign: 'center',
+            flex: 1,
+            alignItems: 'center',
           }}
         >
-          <Link to="/" style={{ display: 'block', lineHeight: 0 }} >
-            <Image style={{ fill: '#FF4136', width: 180 }} src={colorMode === 'dark' ? logoWhite : logoBlack} />
-          </Link>
-        </Text>
+          <Text
+            sx={{
+              fontSize: '24px',
+              color: 'white',
+              fontFamily: 'inter',
+              textAlign: 'center',
+            }}
+          >
+            <Link to="/" style={{ display: 'block', lineHeight: 0 }} >
+              <Image style={{ fill: '#FF4136', width: 180 }} src={colorMode === 'dark' ? logoWhite : logoBlack} />
+            </Link>
+          </Text>
+          { isShowSearch &&
+            <Box
+              m="16px 16px 0px 16px"
+              sx={{
+                width: '76%',
+              }}
+            >
+              <Box
+                sx={{
+                  '@media only screen and (max-width: 916px)': {
+                    display: 'none',
+                  },
+                }}
+              >
+                <Search {...searchCompProps}/>
+              </Box>
+              <Box
+                sx={{
+                  display: 'none',
+                  '@media only screen and (max-width: 916px)': {
+                    display: 'initial',
+                  },
+                }}
+              >
+              </Box>
+            </Box>
+          }
+        </Flex>
         <Flex
+          mb="8px"
           sx={{
             justifyContent: 'space-between',
             alignItems: 'center',
