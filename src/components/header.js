3e56254cd79706ae00332f8c681939715a9969f5
ShadowLegend <lyheng0@gmail.com>
2020-10-08 14:56:45 +0700

    pass search comp prop to search component and conditionally check if should show search

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { Box, Flex, Text, Image } from 'theme-ui'
+
+import Search from './search'
 import mainLogo from '../../src/images/logo-white.svg'
 import GithubIcon from "../images/icons/github.inline.svg"
-const Header = () => {
+
+const Header = ({ isShowSearch, searchCompProps }) => {
   return (
     <Box
       sx={{
@@ -12,7 +15,7 @@ const Header = () => {
       <Flex
         as="header"
         sx={{
-          height: '80px',
+          height: '120px',
           alignItems: 'center',
           justifyContent: 'space-between',
           margin: '0 auto',
@@ -21,19 +24,54 @@ const Header = () => {
           boxShadow: '0 1px 1rem rgba(10, 10, 25, 0.3)',
         }}
       >
-        <Text
+        <Flex
           sx={{
-            fontSize: '24px',
-            color: 'white',
-            fontFamily: 'inter',
-            textAlign: 'center',
+            flex: 1,
+            alignItems: 'center',
           }}
         >
-          <Link to="/" style={{ display: 'block', lineHeight: 0 }} >
-            <Image style={{ fill: '#FF4136', width: 180 }} src={mainLogo} />
-          </Link>
-        </Text>
+          <Text
+            sx={{
+              fontSize: '24px',
+              color: 'white',
+              fontFamily: 'inter',
+              textAlign: 'center',
+            }}
+          >
+            <Link to="/" style={{ display: 'block', lineHeight: 0 }} >
+              <Image style={{ fill: '#FF4136', width: 180 }} src={mainLogo} />
+            </Link>
+          </Text>
+          { isShowSearch &&
+            <Box
+              m="16px 16px 0px 16px"
+              sx={{
+                width: '76%',
+              }}
+            >
+              <Box
+                sx={{
+                  '@media only screen and (max-width: 916px)': {
+                    display: 'none',
+                  },
+                }}
+              >
+                <Search {...searchCompProps}/>
+              </Box>
+              <Box
+                sx={{
+                  display: 'none',
+                  '@media only screen and (max-width: 916px)': {
+                    display: 'initial',
+                  },
+                }}
+              >
+              </Box>
+            </Box>
+          }
+        </Flex>
         <Flex
+          mb="8px"
           sx={{
             justifyContent: 'space-between',
           }}
