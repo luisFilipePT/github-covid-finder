bb7faedf387ddcca243588d4b97d89955a6aca6e
Faisal Amir <urmauur@gmail.com>
2020-10-13 09:23:17 +0700

    Make repoCard and About us more readable, update 404 support theme dark and light

--- src/components/repoCard.js
+++ src/components/repoCard.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Card, Grid, Text, Box } from 'theme-ui'
-import GithubIcon from "../images/icons/github.inline.svg"
-import CodeIcon from "../images/icons/code.inline.svg"
-import IssueIcon from "../images/icons/issue.inline.svg"
-import StarIcon from "../images/icons/star.inline.svg"
-import {logoMapper} from "../data/languageLogoList.js"
-import './layout.css'
-const openGithubPage = (githubLink) => {
-  window.open(githubLink, '_blank');
-};
+import CodeIcon from '../images/icons/code.inline.svg'
+import GithubIcon from '../images/icons/github.inline.svg'
+import IssueIcon from '../images/icons/issue.inline.svg'
+import StarIcon from '../images/icons/star.inline.svg'
+
+const openGithubPage = githubLink => {
+  window.open(githubLink, '_blank')
+}
 
 const RepoCard = ({ repo }) => {
   return (
@@ -22,8 +21,8 @@ const RepoCard = ({ repo }) => {
         borderColor: 'cardBorder',
         padding: '15px 15px 25px 20px',
         ':hover': {
-          cursor: 'pointer'
-        }
+          cursor: 'pointer',
+        },
       }}
       onClick={() => openGithubPage(repo.html_url)}
     >
@@ -33,13 +32,15 @@ const RepoCard = ({ repo }) => {
           height: '80%',
         }}
       >
-        <Text sx={{ fontSize: 22, py: '8px', fontWeight: 'bold' }}>
-          {repo.name}
-        </Text>
-        <Text sx={{ fontSize: 18, py: '8px', color: '#9d1e1e' }}>
+        <Text sx={{ fontSize: 20, py: '8px' }}>{repo.name}</Text>
+        <Text sx={{ fontSize: 16, py: '4px', color: '#9d1e1e' }}>
           {repo.full_name}
         </Text>
-        <Text sx={{ pt: '4px', color: '#805f5f' }}>{repo.description}</Text>
+        <Text
+          sx={{ pt: '4px', color: '#805f5f', lineHeight: '20px', fontSize: 14 }}
+        >
+          {repo.description}
+        </Text>
       </Box>
       <Grid
         columns={4}
@@ -47,52 +48,97 @@ const RepoCard = ({ repo }) => {
           borderTop: 'solid 1px rgba(255, 255, 255, 0.05)',
           paddingTop: 15,
           textAlign: 'center',
+          fontSize: 14,
         }}
       >
-        <Text><span role="img" aria-label="star" style={{ verticalAlign: 'middle' }}>
-          <StarIcon />
-        </span> {repo.stargazers_count}</Text>
-        <Text><span class="tooltip" role="img" aria-label="issues" style={{ verticalAlign: 'middle' }}>
-          <IssueIcon />
-          <span class="tooltiptext">Open Issues</span>
-        </span> {repo.open_issues_count}</Text>
+        <Text>
+          <span
+            role="img"
+            aria-label="star"
+            style={{ verticalAlign: 'middle' }}
+          >
+            <StarIcon />
+          </span>{' '}
+          {repo.stargazers_count}
+        </Text>
+        <Text>
+          <span
+            role="img"
+            aria-label="issues"
+            style={{ verticalAlign: 'middle' }}
+          >
+            <IssueIcon />
+          </span>{' '}
+          {repo.open_issues_count}
+        </Text>
         <a href={repo.html_url} style={{ color: 'currentColor' }}>
-          <Text><span role="img" aria-label="github" style={{ verticalAlign: 'middle' }}>
-            <GithubIcon />
-          </span> Github</Text>
+          <Text>
+            <span
+              role="img"
+              aria-label="github"
+              style={{ verticalAlign: 'middle' }}
+            >
+              <GithubIcon />
+            </span>{' '}
+            Github
+          </Text>
         </a>
-        {displayLogo()}
+        <Text>
+          <span
+            role="img"
+            aria-label="code"
+            style={{ verticalAlign: 'middle' }}
+          >
+            <CodeIcon />
+          </span>{' '}
+          {repo.language}
+        </Text>
       </Grid>
     </Card>
   )
 
-  function displayLogo()
-  { 
-    //check if logoMapper contains the repo.language as a key   
-    if(logoMapper[repo.language]!==undefined)
-    {
-      return(<Text>
-        <span class="tooltip" role="img" aria-label="language" style={{ verticalAlign: 'middle' }}>
-        <img width="16px" height="16px" src={logoMapper[repo.language]} alt={repo.language} />
-        {checkNullLang()}
-        </span>
-        </Text>
-      )//checkNullLang() for tool tip visibility.
-    }
-    //when either repo.language is null or not in logoMapper just displaying code icon with repo.language with no tooltip 
-    else
-    {
+  function displayLogo() {
+    //check if logoMapper contains the repo.language as a key
+    if (logoMapper[repo.language] !== undefined) {
       return (
-      <Text><span role="img" aria-label="code" style={{ verticalAlign: 'middle' }}><CodeIcon /></span> {repo.language}</Text>
-        )
+        <Text>
+          <span
+            class="tooltip"
+            role="img"
+            aria-label="language"
+            style={{ verticalAlign: 'middle' }}
+          >
+            <img
+              width="16px"
+              height="16px"
+              src={logoMapper[repo.language]}
+              alt={repo.language}
+            />
+            {checkNullLang()}
+          </span>
+        </Text>
+      ) //checkNullLang() for tool tip visibility.
+    }
+    //when either repo.language is null or not in logoMapper just displaying code icon with repo.language with no tooltip
+    else {
+      return (
+        <Text>
+          <span
+            role="img"
+            aria-label="code"
+            style={{ verticalAlign: 'middle' }}
+          >
+            <CodeIcon />
+          </span>{' '}
+          {repo.language}
+        </Text>
+      )
     }
   }
 
-  function checkNullLang()
-  {
-    if(repo.language!==null)
-      return(<span class="tooltiptext">{repo.language}</span>)
+  function checkNullLang() {
+    if (repo.language !== null)
+      return <span class="tooltiptext">{repo.language}</span>
   }
 }
 export default RepoCard
-
